1cb5ce6cdd62d12fc8d2aa3cd22ccdde9de9f15b
Somsubhra De <82537035+somsubhra04@users.noreply.github.com>
2022-11-04 22:57:20 +0530

    Update Q1.py

--- python_exam/Q1.py
+++ python_exam/Q1.py
@@ -4,7 +4,7 @@ Enter the solution for Q1 here.
 Note: You may use may define any additional class, functions if necessary.
 However, DO NOT CHANGE THE TEMPLATE CHANGE THE TEMPLATE OF THE FUNCTIONS PROVIDED.
 """
-
+import numpy as np
 
 def Dij_generator():
     """
@@ -16,10 +16,36 @@ def Dij_generator():
     graph_object = None
     try:
         # Enter your code here
+        with open(r"C:\Users\Somsubhra\transit-routing\python_exam\ChicagoSketch_net.tntp") as f:
+            lines = f.readlines()
+        lines = lines[9:]
+        inits = []
+        terms = []
+        lengths = []
+        for i in range(len(lines)):
+            temp = lines[i].split()
+            inits.append(int(temp[0]))
+            terms.append(int(temp[1]))
+            lengths.append(float(temp[4]))
+        
+        dims1 = max(inits)
+        dims2 = max(terms)
+        graph_object = np.ones((dims1, dims2))*np.inf
+
+        for i in range(len(lengths)):
+            graph_object[inits[i]-1][terms[i]-1] = lengths[i]
         return graph_object
     except:
         return graph_object
 
+def min_D(dist, dist_calc):
+    mn = np.inf
+
+    for i in range(dist.shape[0]):
+        if dist_calc[i]==0 and dist[i]<mn:
+            mn = dist[i]
+            min_index = i
+    return min_index
 
 def Q1_dijkstra(source: int, destination: int, graph_object) -> int:
     """
@@ -39,6 +65,25 @@ def Q1_dijkstra(source: int, destination: int, graph_object) -> int:
     shortest_path_distance = -1
     try:
         # Enter your code here
+        try:
+        V = graph_object.shape[1]
+
+        dist = np.ones(V) * np.inf
+        dist[source-1] = 0
+        dist_calc = np.zeros(V)
+        
+        for i in range(V):
+            u = min_D(dist, dist_calc)
+            dist_calc[u] = 1
+            for v in range(V):
+                is_neighbour = graph_object[u][v]!=np.inf
+                if (is_neighbour):
+                    new_dist = dist[u] + graph_object[u][v]
+                    if(dist[v] > new_dist):
+                        dist[v] = new_dist
+        shortest_path_distance = dist[destination-1] if dist[destination-1]!= np.inf else -1
         return shortest_path_distance
     except:
         return shortest_path_distance
+graph_object = Dij_generator()
+print(Q1_dijkstra(253, 127, graph_object))
