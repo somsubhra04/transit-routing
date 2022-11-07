38bf4f6abc04705052d86edd86c681233fc7d030
Somsubhra De <82537035+somsubhra04@users.noreply.github.com>
2022-11-08 01:25:36 +0530

    Update Q3.py

--- python_exam/Q3.py
+++ python_exam/Q3.py
@@ -3,7 +3,14 @@ Enter the solution for Q3 here.
 Note: You may use may define any additional class, functions if necessary.
 However, DO NOT CHANGE THE TEMPLATE CHANGE THE TEMPLATE OF THE FUNCTIONS PROVIDED.
 """
+import numpy as np
 
+def calc_time(st):
+    hrs = int(st[:2])
+    mins = int(st[3:5])
+    secs = int(st[6:])
+
+    return 3600*hrs + 60*mins + secs
 
 def number_of_routes(source_stopid: str, destination_stopid: str) -> int:
     """
@@ -19,6 +26,39 @@ def number_of_routes(source_stopid: str, destination_stopid: str) -> int:
     final_count = -1
     try:
         # Enter your code here
+        with open(__file__[:-5] + "stop_times.txt") as f:
+            lines = f.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i][:-1].split(',')[:4]
+        
+        lines = lines[1:]
+
+        trip_to_stops = {}
+        for i in range(len(lines)):
+            trip_to_stops[lines[i][0]] = []
+    
+        for i in range(len(lines)):
+            trip_to_stops[lines[i][0]].append(lines[i][1])
+
+        with open(__file__[:-5] + "trips.txt") as f:
+            lines = f.readlines()
+        
+        for i in range(len(lines)):
+            lines[i] = lines[i][:-1].split(',')[:2]
+        lines = lines[1:]
+            
+        route_to_stops = {}
+        for i in range(len(lines)):
+            route_to_stops[lines[i][1]] = set()
+        
+        for i in range(len(lines)):
+            stops = trip_to_stops[lines[i][0]]
+            for stop in stops:
+                route_to_stops[lines[i][1]].add(stop)
+        final_count = 0
+        for v in route_to_stops.values():
+            if source_stopid in v and destination_stopid in v:
+                final_count+=1
         return final_count
     except:
         return final_count
